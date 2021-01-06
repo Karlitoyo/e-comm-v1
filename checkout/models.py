62ba4ee5ebcb2f2bbfa464a03654691de58ef08f
Karl <karltimmins@gmail.com>
2021-01-06 19:22:21 +0000

    added checkout views and templates

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,4 @@
+  
 import uuid
 
 from django.db import models
@@ -5,6 +6,7 @@ from django.db.models import Sum
 from django.conf import settings
 
 from products.models import Product
+
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
@@ -35,7 +37,7 @@ class Order(models.Model):
         """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE/100
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
@@ -63,12 +65,11 @@ class OrderLineItem(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Override the original save method to set the order number
-        if it hasn't been set already.
+        Override the original save method to set the lineitem total
+        and update the order total.
         """
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 
     def __str__(self):
         return f'SKU {self.product.sku} on order {self.order.order_number}'
-
